01cc35d912b4082b4e136508530bf7c27a746771
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-07 19:05:57 -0500

    fix linter

--- src/components/DrumKit.vue
+++ src/components/DrumKit.vue
@@ -4,7 +4,7 @@
       <div class="keys">
         <div class="key">
         <button
-          @click.prevent="playSound('http://soundbible.com/mp3/Bike Horn-SoundBible.com-602544869.mp3')"
+          @click.prevent="playSound('http://soundbible.com/mp3/Tinkle-Lisa_Redfern-1916445296.mp3')"
           >
         <kbd>A</kbd>
         <span class="sound">stars</span>
@@ -91,36 +91,36 @@ export default {
   name: 'DrumKit',
   data() {
     return {
-      sounds: { 
-        '65' : 'http://soundbible.com/mp3/Tinkle-Lisa_Redfern-1916445296.mp3',
-        '68' : 'http://soundbible.com/mp3/Demon_Girls_Mockingbir-Hello-1365708396.mp3',
-        '83' : 'http://soundbible.com/mp3/Bike Horn-SoundBible.com-602544869.mp3',
-        '70' : 'http://soundbible.com/mp3/Accordion-SoundBible.com-74362576.mp3',
-        '71' : 'http://soundbible.com/mp3/front-desk-bells-daniel_simon.mp3',
-        '72' : 'http://soundbible.com/mp3/Judges Gavel-SoundBible.com-1321455227.mp3',
-        '74' : 'http://soundbible.com/mp3/Music_Box-Big_Daddy-1389738694.mp3',
-        '75' : 'http://soundbible.com/mp3/Temple Bell Bigger-SoundBible.com-1940249027.mp3',
-        '76' : 'http://soundbible.com/mp3/killdeer_song-Mike_Koenig-1144525481.mp3',
-        '186': 'http://soundbible.com/mp3/Coin_Drop-Willem_Hunt-569197907.mp3'
-      }
+      sounds: {
+        65: 'http://soundbible.com/mp3/Tinkle-Lisa_Redfern-1916445296.mp3',
+        68: 'http://soundbible.com/mp3/Demon_Girls_Mockingbir-Hello-1365708396.mp3',
+        83: 'http://soundbible.com/mp3/Bike Horn-SoundBible.com-602544869.mp3',
+        70: 'http://soundbible.com/mp3/Accordion-SoundBible.com-74362576.mp3',
+        71: 'http://soundbible.com/mp3/front-desk-bells-daniel_simon.mp3',
+        72: 'http://soundbible.com/mp3/Judges Gavel-SoundBible.com-1321455227.mp3',
+        74: 'http://soundbible.com/mp3/Music_Box-Big_Daddy-1389738694.mp3',
+        75: 'http://soundbible.com/mp3/Temple Bell Bigger-SoundBible.com-1940249027.mp3',
+        76: 'http://soundbible.com/mp3/killdeer_song-Mike_Koenig-1144525481.mp3',
+        186: 'http://soundbible.com/mp3/Coin_Drop-Willem_Hunt-569197907.mp3',
+      },
     };
   },
   created() {
     window.addEventListener('keydown', (e) => {
-      if (`data-key="${e.keyCode}"`) {        
-        const link = this.sounds[e.keyCode]
-        this.playSound(link)
+      if (e.keyCode) {
+        const link = this.sounds[e.keyCode];
+        this.playSound(link);
       }
-    })
+    });
   },
 
   methods: {
     playSound(sound) {
       if (sound) {
-       const audio = new Audio(sound);
-       if (!audio) return;
-       audio.currentTime = 0;
-       audio.play();
+        const audio = new Audio(sound);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
       }
     },
   },
@@ -136,8 +136,6 @@ export default {
   height: 750px ;
   width: 100%;
 }
-
-/* }  */
 
 html {
   font-size: 10px;
@@ -174,11 +172,11 @@ body,html {
   text-shadow: 0 0 .5rem black;
 }
 
-/* .playing {
+.playing {
   transform: scale(1.1);
   border-color: #ffc600;
   box-shadow: 0 0 1rem #ffc600;
-} */
+}
 
 kbd {
   display: block;
